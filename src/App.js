0d7ab34f808be097fd3462effaa7fd122fe46906
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-29 18:33:11 +0100

    Set up the route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.scss";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import Cart from "./pages/cart/Cart";
+import Admin from "./pages/admin/Admin";
+import Footer from "./components/footer/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="./contact" element={<Contact />} />
+          <Route path="./cart" element={<Cart />} />
+          <Route path="./admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
