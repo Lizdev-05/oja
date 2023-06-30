ca4009411b3af08dbd8ccd0198995c83d9b7c086
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-30 10:33:46 +0100

    Add the pages as group import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
-import Contact from "./pages/contact/Contact";
-import Cart from "./pages/cart/Cart";
-import Admin from "./pages/admin/Admin";
 import Footer from "./components/footer/Footer";
+// Pages
+import { Home, Contact, Cart, Admin } from "./pages";
 
 const App = () => {
   return (
