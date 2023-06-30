82f53645d94f97d1ee30acc111db03027fffdd38
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-30 12:06:56 +0100

    Add header css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
 import { Home, Contact, Cart, Admin } from "./pages";
 
-// omponent
+// component
 import { Header, Footer } from "./components";
 const App = () => {
   return (
