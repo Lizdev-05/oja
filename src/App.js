797e315059040df7a1c3162ed3837917b937309c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-30 10:36:37 +0100

    Add the componet as group import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 // Pages
 import { Home, Contact, Cart, Admin } from "./pages";
 
+// omponent
+import { Header, Footer } from "./components";
 const App = () => {
   return (
     <>
