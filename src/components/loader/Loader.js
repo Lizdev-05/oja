84f430c076787a74e7095532af98c665e12cc859
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-14 05:26:57 +0000

    Use reactdom to create the portal for the loader we created in index.html

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,16 +1,15 @@
 import React from "react";
 import loaderImg from "../../assets/loader.gif";
 import style from "./Loader.module.scss";
+import ReactDOM from "react-dom";
 
 const Loader = () => {
-  return (
-    (
-      <div className={style.wrapper}>
-        <div className={style.loader}>
-          <img src={loaderImg} alt="loading..." />
-        </div>
+  return ReactDOM.createPortal(
+    <div className={style.wrapper}>
+      <div className={style.loader}>
+        <img src={loaderImg} alt="loading..." />
       </div>
-    ),
+    </div>,
     document.getElementById("loader")
   );
 };
