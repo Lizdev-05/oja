0b613cd845c0c14a983caf74e12fd65e6648140f
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-14 05:48:11 +0000

    Use the useState to manage the state

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,9 +1,11 @@
 import React from "react";
 import loaderImg from "../../assets/loader.gif";
 import style from "./Loader.module.scss";
+
 import ReactDOM from "react-dom";
 
 const Loader = () => {
+  // Use reactdom to create the portal for the loader we created in index.html
   return ReactDOM.createPortal(
     <div className={style.wrapper}>
       <div className={style.loader}>
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,19 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import registerImg from "../../assets/register.png";
 import Card from "../../components/card/Card";
 import style from "./Auth.module.scss";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    console.log(email, password, confirmPassword);
+  };
+
   return (
     <section className={`container ${style.auth}`}>
       <Card>
         <div className={style.form}>
           <h2>Login</h2>
-          <form>
-            <input type="email" placeholder="Enter your email" required />
-            <input type="password" placeholder="Enter password" required />
-            <input type="password" placeholder="Confirm password" required />
+          <form onSubmit={registerUser}>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Enter password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
             <button className="--btn --btn-primary --btn-block">
               Register
             </button>
