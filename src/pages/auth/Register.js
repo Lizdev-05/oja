2db8f736acae7ab9ca4e9b39f7e7bf1adc525403
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-20 06:46:17 +0000

    Complete the registration with firebase

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import registerImg from "../../assets/register.png";
 import Card from "../../components/card/Card";
 import style from "./Auth.module.scss";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import Loader from "../../components/loader/Loader";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const registerUser = (e) => {
     e.preventDefault();
@@ -19,12 +25,35 @@ const Register = () => {
       return;
     }
 
+    setIsLoading(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // once the user has signed up, set the loading state to false
+        setIsLoading(false);
+        toast.success("User registered successfully");
+        // redirect the user to the login page
+        navigate("/login");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+        setIsLoading(false);
+        // ..
+      });
+
     console.log(email, password, confirmPassword);
   };
 
   return (
     <>
       <ToastContainer />
+      {isLoading && <Loader />}
       <section className={`container ${style.auth}`}>
         <Card>
           <div className={style.form}>
