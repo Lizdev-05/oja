9d268c1582d7253a39e27766e99170b69381f6e9
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-12-11 05:23:59 +0000

    Create logic for the next slide arrow

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -5,11 +5,16 @@ import "./Slider.scss";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLenght = sliderData.length;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLenght - 1 ? 0 : currentSlide + 1);
+  };
 
   return (
     <div className="slider">
-      <AiOutlineArrowLeft className="arrow prev" />
-      <AiOutlineArrowRight className="arrow next" />
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
 
       {sliderData.map((slide, index) => {
         const { image, heading, desc } = slide;
