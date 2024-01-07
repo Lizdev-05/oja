3a665467882e12cddae8f0f9f7a15522487280bf
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-07 09:11:04 +0000

    Add the sort mockup

--- src/components/product/productList/ProductList.jsx
+++ src/components/product/productList/ProductList.jsx
@@ -7,7 +7,7 @@ const ProductList = () => {
   const [grid, setGrid] = useState(true);
 
   return (
-    <div className={styles["product-list"]}>
+    <div className={styles["product-list"]} id="product">
       <div className={styles.top}>
         <div className={styles.icons}>
           <BsFillGridFill
@@ -16,6 +16,26 @@ const ProductList = () => {
             onClick={() => setGrid(true)}
           />
           <FaListAlt color="#0066d4" size={22} onClick={() => setGrid(false)} />
+          <p>
+            <b>14</b> products found
+          </p>
+        </div>
+        {/* Search  */}
+        <div>
+          <p>Search</p>
+        </div>
+
+        {/* Sort */}
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="higest-price">Higest Price</option>
+
+            <option value="a-z"> A-Z</option>
+            <option value="z-a">Z-A</option>
+          </select>
         </div>
       </div>
     </div>
