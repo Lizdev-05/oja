d8b38e3a22d68bc2364e088677a44e10afcaf765
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-02 05:52:20 +0000

    Dynamically display the page numbers

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Pagination.module.scss";
 
-export const Pagination = () => {
-  return <div>Pagination</div>;
+export const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  productsPerPage,
+  totalProducts,
+}) => {
+  const pageNumbers = [];
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+  //Limit the number of pages shown in pagination
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  //Limit the number of products shown in each page
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+  //For loop to populate the pageNumbers array with the number of pages
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <ul className={styles.pagination}>
+      <li>prev</li>
+
+      {pageNumbers.map((number) => {
+        return <li key={number}>{number}</li>;
+      })}
+
+      <li>next</li>
+      <p>
+        <b>Page {currentPage}</b>
+        {` of `}
+        <b>{totalPages}</b>
+      </p>
+    </ul>
+  );
 };
