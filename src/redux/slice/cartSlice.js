d23fb87108ba324b5e1f0ed8570314f19070ff3e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-03 06:38:05 +0000

    Export the cart reducers and also add the cart reducer to the store

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -10,11 +10,19 @@ const initialState = {
 };
 
 const cartSlice = createSlice({
-  name: second,
+  name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    ADD_TO_CART: (state, action) => {
+      console.log(action.payload);
+    },
+  },
 });
 
-export const {} = cartSlice.actions;
+export const { ADD_TO_CART } = cartSlice.actions;
+
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartTotalQuantity = (state) => state.cart.cartTotalQuantity;
+export const selectCartTotalAmount = (state) => state.cart.cartTotalAmount;
 
 export default cartSlice.reducer;
