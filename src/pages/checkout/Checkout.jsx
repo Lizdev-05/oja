c2b5b72086d1a913cedaf79a76f881d460cd280f
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-08 12:14:52 +0000

    create the checkout form mockup

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -14,6 +14,7 @@ import {
   selectShippingAddress,
 } from "../../redux/slice/checkoutSlice";
 import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
+import { toast } from "react-toastify";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
