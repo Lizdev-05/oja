9ed6875bccc1084f6b64af16a58d81b6ee56101b
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-10 05:57:01 +0000

    Create the order history

--- src/pages/orderHistory/OrderHistory.js
+++ src/pages/orderHistory/OrderHistory.js
@@ -1,30 +1,85 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./OrderHistory.module.scss";
 import useFetchCollection from "../../customHooks/useFetchCollection";
+import { useDispatch, useSelector } from "react-redux";
+import { STORE_ORDERS, selectOrderHistory } from "../../redux/slice/orderSlice";
+import { selectUserID } from "../../redux/slice/authSlice";
+import Loader from "../../components/loader/Loader";
 
 const OrderHistory = () => {
-  const { data, isLoading, error } = useFetchCollection("orders");
+  const { data, isLoading } = useFetchCollection("orders");
+  const dispatch = useDispatch();
+  const orders = useSelector(selectOrderHistory);
+  const userID = useSelector(selectUserID);
 
-  console.log(data);
+  useEffect(() => {
+    dispatch(STORE_ORDERS(data));
+  }, [data, dispatch]);
+
+  const handleClick = (id) => () => {
+    console.log(id);
+  };
 
   return (
     <section>
-      <div className="container">
-        <h2>Order History</h2>
-        <div className={style.orderHistory}>
-          {data &&
-            data.map((order) => {
-              return (
-                <div key={order.id} className={style.order}>
-                  <h3>Order ID: {order.id}</h3>
-                  <p>Amount: {order.amount}</p>
-                  <p>
-                    Created: {new Date(order.created * 1000).toLocaleString()}
-                  </p>
-                </div>
-              );
-            })}
-        </div>
+      <div className={`container ${style.order}`}>
+        <h2>Your Order History</h2>
+        <p>
+          Open an order to leave a <b>review</b>{" "}
+        </p>
+        <br />
+        <>
+          {isLoading && <Loader />}
+          <div className={style.table}>
+            {orders.length === 0 ? (
+              <p>No order found</p>
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>S/N</th>
+                    <th>Date</th>
+                    <th>Order ID</th>
+                    <th>Order Amount</th>
+                    <th>Order Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order, index) => {
+                    const {
+                      id,
+                      orderDate,
+                      orderTime,
+                      orderAmount,
+                      orderStatus,
+                    } = order;
+                    return (
+                      <tr key={id} onClick={handleClick(id)}>
+                        <td>{index + 1}</td>
+                        <td>
+                          {orderDate} {orderTime}
+                        </td>
+                        <td>{id}</td>
+                        <td>${orderAmount}</td>
+                        <td>
+                          <p
+                            className={
+                              orderStatus !== "Delivered"
+                                ? `${style.pending}`
+                                : `${style.delivered}`
+                            }
+                          >
+                            {orderStatus}
+                          </p>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </>
       </div>
     </section>
   );
