1a0d65515b3e958b41530a7caa91d581c1a42062
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-20 20:49:58 +0000

    add api link

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -43,7 +43,7 @@ const Checkout = () => {
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads. This logic is from the Stripe documentation: https://stripe.com/docs/payments/quickstart?lang=node&client=react
     // fetch("http://localhost:4242/create-payment-intent", {
-    fetch("https://oja.onrender.com/create-payment-intent", {
+    fetch("https://oja-api-db1r.onrender.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
